1afbc540896f16dbf6f51b495a21cb5f676ff20b
Peter Garcia <165866480+pgworksonline@users.noreply.github.com>
2025-08-25 20:12:38 -0600

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,19 +1,20 @@
-# Number guessing game
+# Number guessing game to emulate a do-while loop
 from random import randint
 
 LOW, HIGH = 1, 10
 
 secret_number = randint(LOW, HIGH)
 clue = ""
+number_guessed = False
 
-while True:
-    guess = input(f"Guess a number between {LOW} and {HIGH} {clue} ")
-    number = int(guess)
+while not number_guessed:
+    user_input = input(f"Guess a number between {LOW} and {HIGH} {clue} ")
+    number = int(user_input)
     if number > secret_number:
         clue = f"(less than {number})"
     elif number < secret_number:
         clue = f"(greater than {number})"
     else:
-        break
+        number_guessed = True
 
 print(f"You guessed it! The secret number is {number}")
