2629ff634874192646788189005340fc25c6db8c
Peter Garcia <165866480+pgworksonline@users.noreply.github.com>
2025-11-13 14:35:59 -0700

    Implement list comprehension for final prices calculation
    Added a list comprehension option for calculating final prices.

--- calculating_after_tax.py
+++ calculating_after_tax.py
@@ -1,3 +1,4 @@
+#Getting final prices using map()
 txns = [1.09, 23.56, 56.89, 77.23, 7.77]
 TAX_RATE = 0.08
 
@@ -6,3 +7,7 @@ def get_price_with_tax(txn):
 
 final_prices = map(get_price_with_tax, txns)
 list(final_prices)
+
+#Another option using list comprehension
+final_prices = [get_price_with_tax(i) for i in txns]
+final_prices
